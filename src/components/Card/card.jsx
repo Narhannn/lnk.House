695293674d1bcea830cfd0d17d7f5ca763a1a439
Narhan <narhankushnazarov@gmail.com>
2026-01-09 12:22:49 +0600

    change structure

--- src/components/Card/card.jsx
+++ src/components/Card/card.jsx
@@ -3,15 +3,13 @@
 function Card({picture}) {
   return (
     <>
-      <div className="card">
-        <div className="cards">
+      <div className="cards">
         <img src={picture.img} alt="" />
         <h3>{picture.author}</h3>
-        <h2>Охота Амура</h2>
-        <h4>Холст, масло (50х80) </h4>
-        <h5>14 500 руб</h5>
+        <h2>{picture.title}</h2>
+        <h4>{picture.description}</h4>
+        <h5>{picture.prise} руб</h5>
         <button>В корзину</button>
-      </div>
       </div>
     </>
   );
--- src/components/Reproduction/reprod.jsx
+++ src/components/Reproduction/reprod.jsx
@@ -1,21 +1,28 @@
 import "./reprod.css";
-import Card from "../Card/casd";
-import axios from "axios"
+import Card from "../Card/card";
+import axios from "axios";
 import { useEffect, useState } from "react";
 
-const API = "https://696087c6e7aa517cb7964bb3.mockapi.io/Ink-House"
+const API = "https://696087c6e7aa517cb7964bb3.mockapi.io/Ink-House";
 
 function Reprod() {
-const  [pictures, SetPictures ] = useState([])
-async function getPictures() {
-  const res = await axios.get(API)
-  SetPictures(res.data)
-  
-}
+  const [pictures, SetPictures] = useState([]);
+  const [filtered, setFiltered] = useState([]);
+  async function getPictures() {
+    const res = await axios.get(API);
+    SetPictures(res.data);
+    setFiltered(res.data);
+  }
 
-useEffect(() => {
-  getPictures()
-}, [])
+  useEffect(() => {
+    getPictures();
+  }, []);
+
+  const filterByCountry = (category) => {
+    const filteredData = pictures.filter((obj) => obj.category === category);
+    setFiltered(filteredData);
+  };
+
   return (
     <>
       <section>
@@ -23,14 +30,16 @@ useEffect(() => {
           <div className="reprod">
             <h1>Репродукции</h1>
             <div className="btns">
-              <button>Франция</button>
-              <button>Германия</button>
-              <button>Англия</button>
+              <button onClick={() => filterByCountry("france")}>Франция</button>
+              <button onClick={() => filterByCountry("germany")}>Германия</button>
+              <button onClick={() => filterByCountry("england")}>Англия</button>
             </div>
           </div>
-          {pictures.map((obj) => (
-            <Card picture={obj} key={obj.id}/>
-          ))}
+          <div className="card">
+            {filtered.map((obj) => (
+              <Card picture={obj} key={obj.id} />
+            ))}
+          </div>
         </div>
       </section>
     </>
